1a3442ef46d404b47d42b9ed9daf48518151e3e6
Rizki <rizkihermansyah2388@gmail.com>
2022-01-04 18:36:57 +0700

    Latihan 5.5: Membuat fungsi sederhana

--- learn.js
+++ learn.js
@@ -52,3 +52,60 @@ console.log('false || true =', false || true) //jika expression sebelah kiri set
 console.log('false || false =', false || false) //karena keduanya false
 console.log('!true =', !true)//jika expression yang diberikan setelah dievaluasi menghasilkan nilai true, maka operator akan mengembalikan false
 console.log('!false =', !false) //ika expression yang diberikan setelah dievaluasi menghasilkan nilai false, maka operator akan mengembalikan true.
+
+
+var nh = ["Domba","Lebah","Ayam","Panda","Kuda","Lumba-Lumba"] // memebuat array
+console.log(nh[0]); // Menampikan Array 0
+console.log(nh[1]); // Menampikan Array 1
+console.log(nh[2]); // Menampikan Array 2
+console.log(nh[3]); // Menampikan Array 3
+console.log(nh[4]); // Menampikan Array 4
+console.log(nh[5]); // Menampikan Array 5
+nh[1]="Monyet"; //mengganti array 1 menjadi monyet
+console.log(nh); // menampilkan array terbaru
+
+nh.push("Buaya"); //menambah array baru "buaya"
+console.log(nh); // menampilkan array terbaru
+
+nh.pop("Monyet"); // melihat dan menghapus item dari Array
+console.log(nh); // menampilkan array terbaru
+
+console.log('Panjang Array = ', nh.length); // menghitung panjang Array
+
+nh.unshift("Hiu"); // menambah Hiu di awal Array
+console.log(nh); // menampilkan array terbaru
+
+nh.shift(); // menampilkan dan menghapus item awal dari array
+console.log(nh); // menampilkan array terbaru
+
+console.log(nh.sort()); // mengurutkan Array
+console.log(nh); // menampilkan array terbaru
+
+
+
+let t_pesawat={
+    nama:"Rizki Hermansyah", //membuat variabel nama yang beris
+    kd_tiket:"11223",//membuat variabel kd_tujuan yang beris
+    tujuan:"Jakarta",//membuat variabel tujuan yang beris
+    jumlah_tiket:"3",//membuat variabel jumlah_tiket yang beris
+    harga_tiket:"Rp.500.000",//membuat variabel harga_tiket yang beris
+
+    info_tiket : function(){ //membuat fungsi info_tiket
+        return this.kd_tiket + " " +this.nama + " " + this.tujuan; //menggabungkan kd_tiket, nama, dan tujuan
+   }
+};
+
+console.log(t_pesawat.nama);//menampilkan nama menggunakan metode Dot Notation
+console.log(t_pesawat.kd_tiket)//menampilkan kd_tiketmenggunakan metode Dot Notation
+console.log(t_pesawat.tujuan);//menampilkan tujuan menggunakan metode Dot Notation
+console.log(t_pesawat.jumlah_tiket);//menampilkan jumlah_tiket menggunakan Dot Notation
+console.log(t_pesawat.harga_tiket);//menampilkan harga_tiket menggunakan Dot Notation
+console.log(t_pesawat.info_tiket()); //menampilkan info_tiket menggunakan Dot Notation
+
+
+console.log(t_pesawat["nama"]);//menampilkan nama menggunakan metode Bracket Notation
+console.log(t_pesawat["kd_tiket"]);//menampilkan kd_tiketmenggunakan metode Bracket Notation
+console.log(t_pesawat["tujuan"]);//menampilkan tujuan menggunakan metode Bracket Notation
+console.log(t_pesawat["jumlah_tiket"]);//menampilkan jumlah_tiket menggunakan metode Bracket Notation
+console.log(t_pesawat["harga_tiket"]);//menampilkan harga_tiket menggunakan metode Bracket Notation
+console.log(t_pesawat["info_tiket"]()); //menampilkan info_tiket menggunakan metode Bracket Notation
